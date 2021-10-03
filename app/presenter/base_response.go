7079afd487ff7df1f6d5df8a38bc158e08aef0a2
Adit <adityahimaone@gmail.com>
2021-10-03 21:28:57 +0700

    fix error jwt message

--- app/presenter/base_response.go
+++ app/presenter/base_response.go
@@ -6,18 +6,18 @@ import (
 
 type BaseResponse struct {
 	Meta struct {
-		Status   int      `json:"rc"`
-		Message  string   `json:"message"`
-		Messages []string `json:"messages,omitempty"`
+		Code     int      `json:"code"`
+		Status   string   `json:"message"`
+		Messages string `json:"messages,omitempty"`
 	} `json:"meta"`
 	Data interface{} `json:"data"`
 }
 
 func NewErrorResponse(c echo.Context, status int, err error) error {
 	response := BaseResponse{}
-	response.Meta.Status = status
-	response.Meta.Message = "Something not right"
-	response.Meta.Messages = []string{err.Error()}
+	response.Meta.Status = "Something not right"
+	response.Meta.Code = status
+	response.Meta.Messages = err.Error()
 
 	return c.JSON(status, response)
 }
